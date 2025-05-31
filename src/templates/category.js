6533948114fd26811bb1133434364a2b06e7463c
raniafathallah <raniaahmed7000@gmail.com>
2025-06-01 00:09:02 +0300

    fix category

--- src/templates/category.js
+++ src/templates/category.js
@@ -55,7 +55,8 @@ export default function CategoryTemplate({ data, pageContext }) {
 <div className='div-container'>
 {data.allWpRecipe.nodes.map((item, index) => (
 <div className='rec-item'>
-<img  className='rec-img' src={"http://localhost:8001"+item.image} alt="A dinosaur"  loading="lazy"/>
+  {/* //src={"http://localhost:8001"+item.image}  */}
+<img  className='rec-img'   src={"/gatsby-recipes"+item.image}/> alt="A dinosaur"  loading="lazy"/>
 
         <span key={index}> <Link  className="rec-title" to={"/recipes/"+item.slug}> {item.title}  </Link>  </span>
    
